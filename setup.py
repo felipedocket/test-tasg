392b28cf691170e12e7a54147a81d2500155c01d
felipedocket <felipe.crispim@docket.com.br>
2021-06-14 16:33:05 -0300

    Setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+print("Instalando pacote test-tag")
+
+setup(
+	name='test_tasg',
+	version='0.0.1',
+	description='Hello world',
+	py_modules=[''],
+	entry_points={
+        'console_scripts': [
+            'test_tasg=test_tag.main:hello',
+        ],
+    }
+)
